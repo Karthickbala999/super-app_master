40fb5e56d10acdb0afe4ddbe16023ed4782af5dc
Karthick Bala <karthickb399@gmail.com>
2026-01-24 14:31:39 +0530

    Clean up API configuration comments
    Removed unnecessary comment and adjusted header.

--- superapp-master/src/config/api.config.js
+++ superapp-master/src/config/api.config.js
@@ -1,4 +1,4 @@
-// Centralized API Configuration
+// Centralized API Configuration hello
 // This file manages all API URLs and endpoints dynamically
 
 const API_CONFIG = {
@@ -143,4 +143,4 @@ const API_CONFIG = {
 
 export default API_CONFIG; 
 
-#hello
+
